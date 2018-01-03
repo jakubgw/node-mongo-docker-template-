9afd73d40240e3801a2419c181ff6679944c09b6
Jakub Gwoźdź <jakub.gwozdz@avanade.com>
2018-01-03 15:35:35 +0100

    debug and watch

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,26 @@ const http = require('http')
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://mongodb:27017/";
 
+  
 
-
-
+  
 const port = 3000
 
 
 const requestHandler = (request : any, response: any) => {
   console.log(request.url)
-  // MongoClient.connect(url, function(err:any, db:any) {
-  //   if (err) throw err;
-  //   console.log("Database created!");
+  MongoClient.connect(url, function(err:any, client:any) {
+    if (err) throw err;
     
-  //   db.close();
-  // });
-  response.end('aass!')
+    var db = client.db('test');  
+    db.collection("collectionName").findOne({}, function(err:any, result:any) {
+      if (err) throw err;
+      
+      response.end(result.a.toString())
+      client.close();
+    }); 
+    
+  });
 }
 
 const server = http.createServer(requestHandler)
